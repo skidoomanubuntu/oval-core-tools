ab9749a02d2dfd39edeb7bc5f51de97991e3b4a0
skidoomanubuntu <steve.barriault@canonical.com>
2023-11-05 18:42:30 +0200

    Phase II - the API call returns JSON. But the cve file is empty

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import os, subprocess
+import os, subprocess, time, json
 
 app = FastAPI()
 
@@ -12,10 +12,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def isOlderThanADay(file):
+    fileTime = os.path.getmtime(file)
+    return ((time.time() - fileTime) / 3600 > 24)
+
 @app.get("/")
 def index():
-    msg = "Operating in pwd %s" % os.getcwd()
     os.chdir(os.getenv('SNAP_DATA'))
+    dict = {}
+    needToRefresh = False
+    # Case 1: The PHP files were never generated, so there is nothing to show for
+    file_prefixes = ['cve', 'usn']
+    for prefix in file_prefixes:
+        if not os.path.isfile('%s_stats.php' % prefix):
+            dict[prefix] = ''
+            needToRefresh = True
+        else:
+            if isOlderThanADay('%s_stats.php' % prefix):
+                needToRefresh = True
+            with open('%s_stats.php' % prefix, 'r') as myFile:
+                dict[prefix] = myFile.read()
     print ('now executing in %s' % os.getcwd())
-    subprocess.Popen(['python3', '%s/bin/security_scan.py' % os.getenv('SNAP')])
-    return msg
+    if needToRefresh:
+        subprocess.Popen(['python3', '%s/bin/security_scan.py' % os.getenv('SNAP')])
+    return json.dumps(dict)
